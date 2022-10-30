5dae72a0f0161c44cfbdc822303b3d75d8c469c4
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-10-30 16:16:37 -0300

    Trabalho aula 7

--- Tarefas/tecladoVirtual/js/script.js
+++ Tarefas/tecladoVirtual/js/script.js
@@ -10,14 +10,55 @@ function escrever() {
 
 /*document.getElementById("password").value =document.getElementById(this).value;*/
 
-function num(x){
- // let senha = [6]
-senha = document.getElementById('visor').innerHTML;
+function num(x) {
+  let senha = document.getElementById("visor").innerHTML
+ // let tam = senha.length - 75;
+ let tam = (senha.length - 75)
+ do{
+    document.getElementById("visor").innerHTML = senha + x
+    tam++
+ }while(tam < 6)
+ 
 
-  document.getElementById('visor').innerHTML = senha + x;
+  console.log(senha.length-75)
 
-  
+  //console.log(aux)
+  /*  if (senha.length > 6) {
+    limpar()
+  }*/
+}
+
+function tam1() {
+  let tamanho = document.getElementById("visor").innerHTML
+  document.getElementById("visor").innerHTML = tamanho.substring(
+    6,
+    tamanho.length - 1
+  )
+  console.log(tamanho)
+  console.log(tmn)
+}
+
+function tam() {
+  let tamanho = document.getElementById("visor").innerHTML
+  if (tamanho.length == 7) {
+    document.getElementById("visor").innerHTML = tamanho.substring(
+      0,
+      tamanho.length - 1
+    )
+    limpar()
+  }
+}
+
+function limparTudo() {
+  document.getElementById("visor").innerHTML = ""
+}
+function limpar() {
+  let senha = document.getElementById("visor").innerHTML
+  document.getElementById("visor").innerHTML = senha.substring(
+    0,
+    senha.length - 1
+  )
+}
 /*
 for(let i=0; i> senha.length;i++){
 }*/
-}
