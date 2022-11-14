32cc598ca29eddb7e4955c27671e2698b4c20faa
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-11-13 23:31:20 -0300

    Finalizando atividades da aula 9

--- Tarefas/trabalho 9/js/script.js
+++ Tarefas/trabalho 9/js/script.js
@@ -192,5 +192,17 @@ function fatorial() {
 }
 
 function perdaMassa() {
-  
+  let massa = Number(prompt("Digite a massa do material: ").replace(",", "."))
+  let i
+  for (i = 1; massa > 0.1; i++) {
+    massa = massa * 0.75
+    console.log("Massa=>" + massa)
+    console.log("Tempo =>" + i)
+  }
+  console.log("Tempo =>" + (i - 1))
+
+  let tempo = (i - 1) * 0.5
+  alert(
+    "O tempo necessario para a massa atingir a 0.10 é de " + tempo + " Minutos"
+  )
 }
